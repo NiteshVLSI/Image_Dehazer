da8b49582e1a681cdbc5acc6804cedcc665c08ab
Nitesh Sharma <140998787+NiteshVLSI@users.noreply.github.com>
2024-04-10 23:01:13 +0530

    Add files via upload

--- alpha_beta.py
+++ alpha_beta.py
@@ -1,18 +1,18 @@
 import numpy as np
 from PIL import Image
 
-# Load the image (assuming it's named "image.jpg")
-image_path = "hazed1.jpeg"
+
+image_path = "foggy_image.jpg"
 image = Image.open(image_path)
 
-# Get the pixel data
+
 pixels = image.load()
 
-# Initialize lists to store alpha and beta values
+
 alpha_values = []
 beta_values = []
 
-# Iterate over each pixel
+
 for y in range(image.height):
     for x in range(image.width):
         r, g, b = pixels[x, y]
@@ -21,7 +21,7 @@ for y in range(image.height):
         alpha = r - g
         beta = 0.5 * (r + g) - b
 
-        # Append to the lists
+        
         alpha_values.append(alpha)
         beta_values.append(beta)
 
@@ -34,5 +34,5 @@ std_beta = np.std(beta_values)
 # Calculate CM value
 cm_value = np.sqrt(std_alpha**2 + std_beta**2) + 0.3 * np.sqrt(mean_alpha**2 + mean_beta**2)
 
-# Print the CM value
+
 print(f"CM value: {cm_value:.2f}")
